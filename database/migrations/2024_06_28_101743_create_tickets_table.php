01707c76475e60f4fc2c70ce3d081a5b52b6f45c
Azhar Islam <azharislam21@gmail.com>
2024-06-29 09:49:34 +0530

    note attachment added

---
+++ database/migrations/2024_06_28_101743_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->integer('account_id');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['open', 'closed'])->default('open');
+            $table->timestamps();
+            $table->integer('created_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
