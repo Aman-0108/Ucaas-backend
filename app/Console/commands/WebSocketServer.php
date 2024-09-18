09d9015b3071ffd218c170cd46567ee48c9570a3
Tushar Subhra <tusharsubhraios@gmail.com>
2024-09-18 13:12:22 +0530

    ssl option added

--- app/Console/commands/WebSocketServer.php
+++ app/Console/commands/WebSocketServer.php
@@ -9,6 +9,10 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
+use React\EventLoop\Loop;
+use React\Socket\SocketServer;
+use React\Socket\SecureServer;
+
 class WebSocketServer extends Command
 {
     /**
@@ -42,16 +46,25 @@ class WebSocketServer extends Command
      */
     public function handle()
     {
+        // Use the default loop
+        $loop = Loop::get();
+
+        // Create a regular TCP server
+        $socket = new SecureServer(  
+            new SocketServer($loop, config('services.websocket.port')),  
+            config('services.websocket.ssl')
+        );  
+
         $server = IoServer::factory(
             new HttpServer(
                 new WsServer(
                     new SocketHandler()
                 )
             ),
-            config('services.websocket.port')
+            $socket
         );
 
-        $this->info("WebSocket server started at ws://localhost:" . config('services.websocket.port'));
+        $this->info("WebSocket server started at wss://localhost:" . config('services.websocket.port'));
 
         $server->run();
 
