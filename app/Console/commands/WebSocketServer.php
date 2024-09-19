1fb88c826d8f40dc353e640f014c1e530a3ceb3a
Tushar Subhra <tusharsubhraios@gmail.com>
2024-09-19 15:00:53 +0530

    ssl code added

--- app/Console/commands/WebSocketServer.php
+++ app/Console/commands/WebSocketServer.php
@@ -9,6 +9,10 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
+// use React\EventLoop\Loop;
+// use React\Socket\SocketServer;
+// use React\Socket\SecureServer;
+
 class WebSocketServer extends Command
 {
     /**
@@ -54,6 +58,26 @@ class WebSocketServer extends Command
         );
 
         $server->run();
+
+        $app = new HttpServer(
+            new WsServer(
+                new SocketHandler()
+            )
+        );
+
+         // Use the default loop
+        //  $loop = Loop::get();
+        // Create a WebSocket server
+        // $webSock = new SocketServer('0.0.0.0:8080', [], $loop);
+
+        // $webSock = new SecureServer($webSock, $loop, [
+        //     'local_cert' => 'CRT_PATH', 
+        //     'local_pk'=> 'KEY_PATH', 
+        //     'allow_self_signed' => true, 
+        //     'verify_peer' => false
+        // ]);
+
+        // $webSock = new IoServer($app, $webSock, $loop);
 
         return Command::SUCCESS;
     }
